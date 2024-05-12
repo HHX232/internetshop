a9fa2227f72df7ef3e6a6a8ee15e6973a236e2ab
HHX232 <140748811+HHX232@users.noreply.github.com>
2024-05-12 15:01:36 +0300

    9 comm

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+
+$(document).ready(function(){
+	$('.trust__slider').slick();
+ })
+ document.getElementById('file').addEventListener('change', function () {
+	var files = this.files;
+	if (files.length > 0) {
+		 var label = document.querySelector('.file__label');
+		 if (files.length === 1) {
+			  label.textContent = files[0].name;
+		 } else {
+			  label.textContent = files.length + " файлов прикреплено";
+		 }
+	}
+});
+
+
 const burgerContent = document.querySelector('.burger__content')
 const burgerIcon = document.querySelector('.burger')
 const burgerClose = document.querySelector('.burger__close-btn')
@@ -29,22 +46,6 @@ let swiper = new Swiper(".trust__comm-first", {
 	  prevEl: ".swiper-button-prev",
 	},
  });
-
- $(document).ready(function(){
-	$('.trust__slider').slick();
- })
- document.getElementById('file').addEventListener('change', function () {
-	var files = this.files;
-	if (files.length > 0) {
-		 var label = document.querySelector('.file__label');
-		 if (files.length === 1) {
-			  label.textContent = files[0].name;
-		 } else {
-			  label.textContent = files.length + " файлов прикреплено";
-		 }
-	}
-});
-
 
 
 
